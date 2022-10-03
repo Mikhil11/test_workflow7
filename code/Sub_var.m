1a60e25211bc3dd8c11c617c85db427a47c6d579
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2022-10-03 15:43:37 +0530

    Add files via upload

---
+++ code/Sub_var.m
@@ -0,0 +1,4 @@
+function c = Sub_var(a,b)    
+%% SUB_VAR This Function subtracts two variables
+    c = a-b;
+end
---
+++ code/add_var.m
@@ -0,0 +1,4 @@
+function c = add_var(a,b)    
+%% ADD_VAR This Function adds two variables
+    c = a+b
+end
